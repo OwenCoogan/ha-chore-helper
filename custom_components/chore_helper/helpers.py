0e3d7c77f39c25b27cc03deef361050665ce15b1
Owen Coogan <owencoogan01@gmail.com>
2024-10-08 23:21:03 +0200

    fix: parse optional date method

--- custom_components/chore_helper/helpers.py
+++ custom_components/chore_helper/helpers.py
@@ -5,22 +5,20 @@ from __future__ import annotations
 
 from datetime import date, datetime
 from typing import Any
+import logging
 
 import homeassistant.util.dt as dt_util
 import voluptuous as vol
 from dateutil.parser import ParserError, parse
 
+_LOGGER = logging.getLogger(__name__)
 
 def now() -> datetime:
     """Return current date and time. Needed for testing."""
     return dt_util.now()
 
-
 def to_date(day: Any) -> date:
-    """Convert datetime or text to date, if not already datetime.
-
-    Used for the first date for every_n_days (configured as text).
-    """
+    """Convert datetime or text to date, if not already datetime."""
     if day is None:
         raise ValueError
     if isinstance(day, date):
@@ -29,14 +27,12 @@ def to_date(day: Any) -> date:
         return day.date()
     return date.fromisoformat(day)
 
-
 def parse_datetime(text: str) -> datetime | None:
     """Parse text to datetime object."""
     try:
         return parse(text)
     except (ParserError, TypeError):
         return None
-
 
 def dates_to_texts(dates: list[date]) -> list[str]:
     """Convert list of dates to texts."""
@@ -48,7 +44,6 @@ def dates_to_texts(dates: list[date]) -> list[str]:
             continue
     return converted
 
-
 def time_text(value: Any) -> str:
     """Have to store time as text - datetime is not JSON serializable."""
     if value is None or value == "":
@@ -58,7 +53,6 @@ def time_text(value: Any) -> str:
     except ValueError as error:
         raise vol.Invalid(f"Invalid date: {value}") from error
 
-
 def month_day_text(value: Any) -> str:
     """Validate format month/day."""
     if value is None or value == "":
@@ -67,3 +61,13 @@ def month_day_text(value: Any) -> str:
         return datetime.strptime(value, "%m/%d").date().strftime("%m/%d")
     except ValueError as error:
         raise vol.Invalid(f"Invalid date: {value}") from error
+
+def parse_optional_date(attributes: dict[str, Any], key: str) -> datetime | None:
+    """Parse an optional date from the attributes dictionary."""
+    date_str = attributes.get(key)
+    if date_str:
+        try:
+            return datetime.fromisoformat(date_str)
+        except ValueError as e:
+            _LOGGER.error(f"Error parsing date '{date_str}' from key '{key}': {e}")
+    return None
